21f504a592f32c2626fb363cdb3408735dc8fd07
j-parthiv <parthivjani9@gmail.com>
2024-04-21 23:51:45 +0200

    refactor: change api to accept image files

--- routes/campground.js
+++ routes/campground.js
@@ -3,15 +3,22 @@ const router = express.Router();
 const catchAsync = require("../utils/catchAsync");
 const { isLoggedIn, isAuthor, validateCampground } = require("../middleware");
 const campgrounds = require("../controllers/campgrounds");
-
+const multer = require("multer");
+const { storage } = require("../cloudinary");
+const upload = multer({ storage });
 router
   .route("/")
   .get(catchAsync(campgrounds.index))
-  .post(
-    isLoggedIn,
-    validateCampground,
-    catchAsync(campgrounds.createCampground)
-  );
+  // .post(
+  //   isLoggedIn,
+  //   validateCampground,
+  //   catchAsync(campgrounds.createCampground)
+  // );
+  .post(upload.array("image"), (req, res) => {
+    console.log(req.body, req.files);
+    res.send("It worked");
+
+  });
 
 router.get("/new", isLoggedIn, campgrounds.renderNewForm);
 
