a6e793a2f137c0d6ebda6cd04c1160b066557fd2
j-parthiv <parthivjani9@gmail.com>
2024-04-22 16:13:40 +0200

    feat: add middleware to get multiple image files

--- routes/campground.js
+++ routes/campground.js
@@ -6,19 +6,16 @@ const campgrounds = require("../controllers/campgrounds");
 const multer = require("multer");
 const { storage } = require("../cloudinary");
 const upload = multer({ storage });
+
 router
   .route("/")
   .get(catchAsync(campgrounds.index))
-  // .post(
-  //   isLoggedIn,
-  //   validateCampground,
-  //   catchAsync(campgrounds.createCampground)
-  // );
-  .post(upload.array("image"), (req, res) => {
-    console.log(req.body, req.files);
-    res.send("It worked");
-
-  });
+  .post(
+    isLoggedIn,
+    upload.array("image"),
+    validateCampground,
+    catchAsync(campgrounds.createCampground)
+  );
 
 router.get("/new", isLoggedIn, campgrounds.renderNewForm);
 
@@ -28,6 +25,7 @@ router
   .put(
     isLoggedIn,
     isAuthor,
+    upload.array("image"),
     validateCampground,
     catchAsync(campgrounds.updateCampground)
   )
